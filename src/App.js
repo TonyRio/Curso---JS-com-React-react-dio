a813fdff3d2c3b0226ba005b8c8b1bfe99e4b981
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-27 19:45:40 -0300

    ex01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Item from "./components/item"
 const App = ()=> {
   return(
-    <h1>Alo, mundo !!</h1>
-
+    <>
+    <h1>Minha primeira aplicação com React</h1>
+    <ul>
+      <Item>item 1</Item>
+      <Item>Item 2</Item>
+      <Item>Item 3</Item>
+      
+    </ul>
+</>
   )
 }
 export default App;
